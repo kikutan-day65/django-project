48e078aa8a30a4a47800cd33c6d7cdbd0d1ef8fa
Haruto Mori <baystarsman@outlook.jp>
2023-02-28 20:19:44 +0100

    how to display the name in the main page dinamically

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    name = 'Haruto'
+    return render(request, 'index.html', {'name': name})

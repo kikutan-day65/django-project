089f2f3a7b48c9eb0c7dc203f44a001f1b3e2aeb
Haruto Mori <baystarsman@outlook.jp>
2023-02-28 20:24:41 +0100

    displays few words in the main page dinamically with *context dictionary*

--- myapp/views.py
+++ myapp/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    name = 'Haruto'
-    return render(request, 'index.html', {'name': name})
+    context = {
+        'name': 'Haruto',
+        'age': 25,
+        'nationality': 'Japan'
+    }
+    return render(request, 'index.html', context)

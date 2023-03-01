e83c6c01be41a79ba4d46d2474260f68d074f7e1
Haruto Mori <baystarsman@outlook.jp>
2023-03-01 19:40:28 +0100

    create new database and how to set it up

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # this file is used to create and manage database
 # Create your models here.
 
-class Feature:
-    id: int
-    name: str
-    detail: str
-    is_true: bool
+"""
+    if you created new models, follow these steps below:
+        1. py manage.py makemigrations 
+        2. py manage.py migrate
+"""
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    detail = models.CharField(max_length=500)

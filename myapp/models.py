a796c20bf7fd7735f72b7bc6d5ee01e67b5c98cd
Haruto Mori <baystarsman@outlook.jp>
2023-03-01 15:13:40 +0100

    model.py is used for database. use data in the html dianmically

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+# this file is used to create and manage database
 # Create your models here.
+
+class Feature:
+    id: int
+    name: str
+    detail: str
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 1
+    feature1.name = 'HARUTO MORI'
+    feature1.details = 'Japanese society'
+
+
+    feature2 = Feature()
+    feature2.id = 2
+    feature2.name = 'YAZAN GHUNAIM'
+    feature2.details = 'Jordanian society'
+
+    feature3 = Feature()
+    feature3.id = 3
+    feature3.name = 'JOSE LOPEZ'
+    feature3.details = 'Middle american society'
+
+    features = [
+        feature1,
+        feature2,
+        feature3,
+    ]
+
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     text = request.POST['text']
